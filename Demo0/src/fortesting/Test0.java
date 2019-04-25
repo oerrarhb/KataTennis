ddae053501a7ec25ac4ba7e9e59f3791458db7fb
Jhon Othmane ERRARHBI <othmane.errarhbi@gmail.com>
2019-04-25 11:50:38 +0200

    Second C

--- Demo0/src/fortesting/Test0.java
+++ Demo0/src/fortesting/Test0.java
@@ -5,6 +5,7 @@ public class Test0 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello , I'm testing Github");
+		System.out.println("I'm testing Branch 2");
 
 	}
 
